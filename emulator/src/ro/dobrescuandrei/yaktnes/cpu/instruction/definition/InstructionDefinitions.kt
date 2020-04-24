b74876fe73029f2dc6de085ac262a2d352b524ff
Andrei Dobrescu <usingvirtualbox@gmail.com>
2020-04-24 15:50:32 +0300

    loading instruction definitions via reflection magic

--- emulator/src/ro/dobrescuandrei/yaktnes/cpu/instruction/definition/InstructionDefinitions.kt
+++ emulator/src/ro/dobrescuandrei/yaktnes/cpu/instruction/definition/InstructionDefinitions.kt
@@ -1,69 +1,17 @@
 package ro.dobrescuandrei.yaktnes.cpu.instruction.definition
 
+import org.reflections.Reflections
 import ro.dobrescuandrei.yaktnes.cpu.instruction.addressing_mode.AddressingMode
-import ro.dobrescuandrei.yaktnes.cpu.instruction.definition.opcodes.*
 
 object InstructionDefinitions
 {
-    private val groupDefinitions = listOf(
-        //REFERENCE: http://www.6502.org/tutorials/6502opcodes.html
-        ADCInstructionDefinitionFactory.newInstance(),
-        ANDInstructionDefinitionFactory.newInstance(),
-        ASLInstructionDefinitionFactory.newInstance(),
-        BCCInstructionDefinitionFactory.newInstance(),
-        BCSInstructionDefinitionFactory.newInstance(),
-        BEQInstructionDefinitionFactory.newInstance(),
-        BITInstructionDefinitionFactory.newInstance(),
-        BMIInstructionDefinitionFactory.newInstance(),
-        BNEInstructionDefinitionFactory.newInstance(),
-        BPLInstructionDefinitionFactory.newInstance(),
-        BRKInstructionDefinitionFactory.newInstance(),
-        BVCInstructionDefinitionFactory.newInstance(),
-        BVSInstructionDefinitionFactory.newInstance(),
-        CLCInstructionDefinitionFactory.newInstance(),
-        CLDInstructionDefinitionFactory.newInstance(),
-        CLIInstructionDefinitionFactory.newInstance(),
-        CLVInstructionDefinitionFactory.newInstance(),
-        CMPInstructionDefinitionFactory.newInstance(),
-        CPXInstructionDefinitionFactory.newInstance(),
-        CPYInstructionDefinitionFactory.newInstance(),
-        DECInstructionDefinitionFactory.newInstance(),
-        DEXInstructionDefinitionFactory.newInstance(),
-        DEYInstructionDefinitionFactory.newInstance(),
-        EORInstructionDefinitionFactory.newInstance(),
-        INCInstructionDefinitionFactory.newInstance(),
-        INXInstructionDefinitionFactory.newInstance(),
-        INYInstructionDefinitionFactory.newInstance(),
-        JMPInstructionDefinitionFactory.newInstance(),
-        JSRInstructionDefinitionFactory.newInstance(),
-        LDAInstructionDefinitionFactory.newInstance(),
-        LDXInstructionDefinitionFactory.newInstance(),
-        LDYInstructionDefinitionFactory.newInstance(),
-        LSRInstructionDefinitionFactory.newInstance(),
-        NOPInstructionDefinitionFactory.newInstance(),
-        ORAInstructionDefinitionFactory.newInstance(),
-        PHAInstructionDefinitionFactory.newInstance(),
-        PHPInstructionDefinitionFactory.newInstance(),
-        PLAInstructionDefinitionFactory.newInstance(),
-        PLPInstructionDefinitionFactory.newInstance(),
-        ROLInstructionDefinitionFactory.newInstance(),
-        RORInstructionDefinitionFactory.newInstance(),
-        RTIInstructionDefinitionFactory.newInstance(),
-        RTSInstructionDefinitionFactory.newInstance(),
-        SBCInstructionDefinitionFactory.newInstance(),
-        SECInstructionDefinitionFactory.newInstance(),
-        SEDInstructionDefinitionFactory.newInstance(),
-        SEIInstructionDefinitionFactory.newInstance(),
-        STAInstructionDefinitionFactory.newInstance(),
-        STXInstructionDefinitionFactory.newInstance(),
-        STYInstructionDefinitionFactory.newInstance(),
-        TAXInstructionDefinitionFactory.newInstance(),
-        TAYInstructionDefinitionFactory.newInstance(),
-        TSXInstructionDefinitionFactory.newInstance(),
-        TXAInstructionDefinitionFactory.newInstance(),
-        TXSInstructionDefinitionFactory.newInstance(),
-        TYAInstructionDefinitionFactory.newInstance()
-    ) as List<InstructionGroupDefinition<Any>>
+    //REFERENCE: http://www.6502.org/tutorials/6502opcodes.html
+    private val groupDefinitions by lazy {
+        Reflections(this::class.java.`package`.name)
+            .getSubTypesOf(InstructionGroupDefinitionFactory::class.java)
+            .map { it.getField("INSTANCE").get(null) as InstructionGroupDefinitionFactory<Any> }
+            .map { factory -> factory.newInstance() }
+    }
 
     init
     {
